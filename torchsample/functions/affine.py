9c463e9feb288920f78a67f12c8849f1b291bfd4
ncullen93 <ncullen@modv-vlan533.0658.apn.wlan.wireless-pennnet.upenn.edu>
2017-04-25 11:55:11 -0400

    affine 2d/3d autograd functions

--- torchsample/functions/affine.py
+++ torchsample/functions/affine.py
@@ -3,31 +3,32 @@ import torch
 import torch.nn.functional as F
 from torch.autograd import Variable
 
-from ..utils import th_iterproduct, th_gather_nd
+from ..utils import th_iterproduct, th_flatten
 
 def F_affine_2d(x, matrix, center=True):
     """
     2D Affine image transform on torch.autograd.Variable
     """
-    # grab A and b weights from 2x3 matrix
     A = matrix[:2,:2]
     b = matrix[:2,2]
 
     # make a meshgrid of normal coordinates
-    coords = Variable(th_iterproduct(x.size(0),x.size(1)), requires_grad=False)
+    #coords = th_iterproduct_like(x).float()
+    coords = Variable(th_iterproduct(x.size(1),x.size(2)).float(), 
+                requires_grad=False)
 
     if center:
         # shift the coordinates so center is the origin
-        coords[:,0] = coords[:,0] - (x.size(0) / 2. + 0.5)
-        coords[:,1] = coords[:,1] - (x.size(1) / 2. + 0.5)
-
+        coords[:,0] = coords[:,0] - (x.size(1) / 2. + 0.5)
+        coords[:,1] = coords[:,1] - (x.size(2) / 2. + 0.5)
+    
     # apply the coordinate transformation
     new_coords = F.linear(coords, A, b)
 
     if center:
         # shift the coordinates back so origin is origin
-        new_coords[:,0] = new_coords[:,0] + (x.size(0) / 2. + 0.5)
-        new_coords[:,1] = new_coords[:,1] + (x.size(1) / 2. + 0.5)
+        new_coords[:,0] = new_coords[:,0] + (x.size(1) / 2. + 0.5)
+        new_coords[:,1] = new_coords[:,1] + (x.size(2) / 2. + 0.5)
 
     # map new coordinates using bilinear interpolation
     x_transformed = F_bilinear_interp_2d(x, new_coords)
@@ -39,153 +40,124 @@ def F_bilinear_interp_2d(input, coords):
     """
     bilinear interpolation of 2d torch.autograd.Variable
     """
-    xc = torch.clamp(coords[:,0], 0, input.size(0)-1)
-    yc = torch.clamp(coords[:,1], 0, input.size(1)-1)
-    coords = torch.stack([xc,yc],1)
+    # take clamp then floor/ceil of x coords
+    x = torch.clamp(coords[:,0], 0, input.size(1)-2).contiguous().view(-1)
+    x0 = x.floor()
+    x1 = x0 + 1
+    # take clamp then floor/ceil of y coords
+    y = torch.clamp(coords[:,1], 0, input.size(2)-2).contiguous().view(-1)
+    y0 = y.floor()
+    y1 = y0 + 1
 
-    coords_lt = coords.floor().long()
-    coords_rb = coords.ceil().long()
-    coords_lb = torch.stack([coords_lt[:, 0], coords_rb[:, 1]], 1)
-    coords_rt = torch.stack([coords_rb[:, 0], coords_lt[:, 1]], 1)
+    stride = torch.LongTensor(input.stride()[1:])
+    x0_ix = x0.mul(stride[0]).long()
+    x1_ix = x1.mul(stride[0]).long()
+    y0_ix = y0.mul(stride[1]).long()
+    y1_ix = y1.mul(stride[1]).long()
 
-    vals_lt = th_gather_nd(input,  coords_lt.detach())
-    vals_rb = th_gather_nd(input,  coords_rb.detach())
-    vals_lb = th_gather_nd(input,  coords_lb.detach())
-    vals_rt = th_gather_nd(input,  coords_rt.detach())
+    input_flat = th_flatten(input)
 
-    coords_offset_lt = coords - coords_lt.type(coords.data.type())
+    vals_00 = input_flat[x0_ix.add(y0_ix).detach()]
+    vals_10 = input_flat[x1_ix.add(y0_ix).detach()]
+    vals_01 = input_flat[x0_ix.add(y1_ix).detach()]
+    vals_11 = input_flat[x1_ix.add(y1_ix).detach()]
 
-    vals_t = vals_lt + (vals_rt - vals_lt) * coords_offset_lt[:, 0]
-    vals_b = vals_lb + (vals_rb - vals_lb) * coords_offset_lt[:, 0]
-    mapped_vals = vals_t + (vals_b - vals_t) * coords_offset_lt[:, 1]
-    return mapped_vals.view_as(input)
+    xd = x - x0
+    yd = y - y0
+    xm1 = 1 - xd
+    ym1 = 1 - yd
+
+    x_mapped = (vals_00.mul(xm1).mul(ym1) +
+                vals_10.mul(xd).mul(ym1) +
+                vals_01.mul(xm1).mul(yd) +
+                vals_11.mul(xd).mul(yd))
+
+    return x_mapped.view_as(input)
 
 
 def F_affine_3d(x, matrix, center=True):
-    # grab A and b weights from 2x3 matrix
     A = matrix[:3,:3]
     b = matrix[:3,3]
 
     # make a meshgrid of normal coordinates
-    coords = Variable(th_iterproduct(x.size(0),x.size(1),x.size(2)), 
-                      requires_grad=False)
+    coords = Variable(th_iterproduct(x.size(1),x.size(2),x.size(3)).float(),
+                requires_grad=False)
 
     if center:
         # shift the coordinates so center is the origin
-        coords[:,0] = coords[:,0] - (x.size(0) / 2. + 0.5)
-        coords[:,1] = coords[:,1] - (x.size(1) / 2. + 0.5)
-        coords[:,2] = coords[:,2] - (x.size(2) / 2. + 0.5)
+        coords[:,0] = coords[:,0] - (x.size(1) / 2. + 0.5)
+        coords[:,1] = coords[:,1] - (x.size(2) / 2. + 0.5)
+        coords[:,2] = coords[:,2] - (x.size(3) / 2. + 0.5)
 
+    
     # apply the coordinate transformation
     new_coords = F.linear(coords, A, b)
 
     if center:
         # shift the coordinates back so origin is origin
-        new_coords[:,0] = new_coords[:,0] + (x.size(0) / 2. + 0.5)
-        new_coords[:,1] = new_coords[:,1] + (x.size(1) / 2. + 0.5)
-        new_coords[:,2] = new_coords[:,2] + (x.size(2) / 2. + 0.5)
+        new_coords[:,0] = new_coords[:,0] + (x.size(1) / 2. + 0.5)
+        new_coords[:,1] = new_coords[:,1] + (x.size(2) / 2. + 0.5)
+        new_coords[:,2] = new_coords[:,2] + (x.size(3) / 2. + 0.5)
 
     # map new coordinates using bilinear interpolation
-    x_transformed = F_bilinear_interp_3d(x, new_coords)
+    x_transformed = F_trilinear_interp_3d(x, new_coords)
 
     return x_transformed
 
 
-def F_bilinear_interp_3d(input, coords):
+def F_trilinear_interp_3d(input, coords):
     """
     trilinear interpolation of 3D image
     """
-    x = torch.clamp(coords[:,0], 0, input.size(0)-1.00001)
-    x0 = x.floor().long()
+    # take clamp then floor/ceil of x coords
+    x = torch.clamp(coords[:,0], 0, input.size(1)-2)
+    x0 = x.floor()
     x1 = x0 + 1
-
-    y = torch.clamp(coords[:,1], 0, input.size(1)-1.00001)
-    y0 = y.floor().long()
+    # take clamp then floor/ceil of y coords
+    y = torch.clamp(coords[:,1], 0, input.size(2)-2)
+    y0 = y.floor()
     y1 = y0 + 1
-
-    z = torch.clamp(coords[:,2], 0, input.size(2)-1.00001)
-    z0 = z.floor().long()
+    # take clamp then floor/ceil of z coords
+    z = torch.clamp(coords[:,2], 0, input.size(3)-2)
+    z0 = z.floor()
     z1 = z0 + 1
 
-    c_000 = torch.stack([x0,y0,z0])
-    c_111 = torch.stack([x1,y1,z1])
-    c_001 = torch.stack([x0,y0,z1])
-    c_010 = torch.stack([x0,y1,z0])
-    c_011 = torch.stack([x0,y1,z1])
-    c_100 = torch.stack([x1,y0,z0])
-    c_110 = torch.stack([x1,y1,z0])
-    c_101 = torch.stack([x1,y0,z1])
+    stride = torch.LongTensor(input.stride())[1:]
+    x0_ix = x0.mul(stride[0]).long()
+    x1_ix = x1.mul(stride[0]).long()
+    y0_ix = y0.mul(stride[1]).long()
+    y1_ix = y1.mul(stride[1]).long()
+    z0_ix = z0.mul(stride[2]).long()
+    z1_ix = z1.mul(stride[2]).long()
 
-    vals_000 = th_gather_nd(input, c_000.detach())
-    vals_111 = th_gather_nd(input, c_111.detach())
-    vals_001 = th_gather_nd(input, c_001.detach())
-    vals_010 = th_gather_nd(input, c_010.detach())
-    vals_011 = th_gather_nd(input, c_011.detach())
-    vals_100 = th_gather_nd(input, c_100.detach())
-    vals_110 = th_gather_nd(input, c_110.detach())
-    vals_101 = th_gather_nd(input, c_101.detach())
+    input_flat = th_flatten(input)
 
-    xd = ((x-x0)/(x1-x0))
-    yd = (y-y0)/(y1-y0)
-    zd = (z-z0)/(z1-z0)
+    vals_000 = input_flat[x0_ix.add(y0_ix).add(z0_ix).detach()]
+    vals_100 = input_flat[x1_ix.add(y0_ix).add(z0_ix).detach()]
+    vals_010 = input_flat[x0_ix.add(y1_ix).add(z0_ix).detach()]
+    vals_001 = input_flat[x0_ix.add(y0_ix).add(z1_ix).detach()]
+    vals_101 = input_flat[x1_ix.add(y0_ix).add(z1_ix).detach()]
+    vals_011 = input_flat[x0_ix.add(y1_ix).add(z1_ix).detach()]
+    vals_110 = input_flat[x1_ix.add(y1_ix).add(z0_ix).detach()]
+    vals_111 = input_flat[x1_ix.add(y1_ix).add(z1_ix).detach()]
 
-    c00 = vals_000*(1-xd) + vals_100*xd
-    c01 = vals_001*(1-xd) + vals_101*xd
-    c10 = vals_010*(1-xd) + vals_110*xd
-    c11 = vals_011*(1-xd) + vals_111*xd
+    xd = x - x0
+    yd = y - y0
+    zd = z - z0
+    xm1 = 1 - xd
+    ym1 = 1 - yd
+    zm1 = 1 - zd
 
-    c0 = c00*(1-yd) + c10*yd
-    c1 = c01*(1-yd) + c11*yd
+    x_mapped = (vals_000.mul(xm1).mul(ym1).mul(zm1) +
+                vals_100.mul(xd).mul(ym1).mul(zm1) +
+                vals_010.mul(xm1).mul(yd).mul(zm1) +
+                vals_001.mul(xm1).mul(ym1).mul(zd) +
+                vals_101.mul(xd).mul(ym1).mul(zd) +
+                vals_011.mul(xm1).mul(yd).mul(zd) +
+                vals_110.mul(xd).mul(yd).mul(zm1) +
+                vals_111.mul(xd).mul(yd).mul(zd))
 
-    c = c0*(1-zd) + c1*zd
-
-    return c.view_as(input)
+    return x_mapped.view_as(input)
 
 
-def F_bicubic_interp_2d(input, coords):
-    def cubic_hermite(A, B, C, D, t):
-        a = -A / 2.0 + (3.0*B) / 2.0 - (3.0*C) / 2.0 + D / 2.0
-        b = A - (5.0*B) / 2.0 + 2.0*C - D / 2.0
-        c = -A / 2.0 + C / 2.0
-        d = B
-        return a*t*t*t + b*t*t + c*t + d
-
-    u = coords[:,0]
-    v = coords[:,0]
-
-    x = u - 0.5
-    xint = x.long()
-    xfract = x - x.floor()
-
-    y = v - 0.5
-    yint = y.long()
-    yfract = y - y.floor()
-
-    p00 = th_gather_nd(input, torch.stack([xint - 1, yint - 1]))
-    p10 = th_gather_nd(input, torch.stack([xint + 0, yint - 1]))
-    p20 = th_gather_nd(input, torch.stack([xint + 1, yint - 1]))
-    p30 = th_gather_nd(input, torch.stack([xint + 2, yint - 1]))
-
-    p01 = th_gather_nd(input, torch.stack([xint - 1, yint + 0]))
-    p11 = th_gather_nd(input, torch.stack([xint + 0, yint + 0]))
-    p21 = th_gather_nd(input, torch.stack([xint + 1, yint + 0]))
-    p31 = th_gather_nd(input, torch.stack([xint + 2, yint + 0]))
- 
-    p02 = th_gather_nd(input, torch.stack([xint - 1, yint + 1]))
-    p12 = th_gather_nd(input, torch.stack([xint + 0, yint + 1]))
-    p22 = th_gather_nd(input, torch.stack([xint + 1, yint + 1]))
-    p32 = th_gather_nd(input, torch.stack([xint + 2, yint + 1]))
- 
-    p03 = th_gather_nd(input, torch.stack([xint - 1, yint + 2]))
-    p13 = th_gather_nd(input, torch.stack([xint + 0, yint + 2]))
-    p23 = th_gather_nd(input, torch.stack([xint + 1, yint + 2]))
-    p33 = th_gather_nd(input, torch.stack([xint + 2, yint + 2]))
-
-    col0 = cubic_hermite(p00,p10,p20,p30,xfract)
-    col1 = cubic_hermite(p01,p11,p21,p31,xfract)
-    col2 = cubic_hermite(p02,p12,p22,p32,xfract)
-    col3 = cubic_hermite(p03,p13,p23,p33,xfract)
-
-    x_mapped = cubic_hermite(col0,col1,col2,col3,yfract)
-    return x_mapped
 
