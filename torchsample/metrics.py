7e2f4868629a266d2336135118911ef858189cf8
ncullen93 <ncullen@modv-vlan533.0527.apn.wlan.wireless-pennnet.upenn.edu>
2017-04-28 10:30:27 -0400

    fixed fit_loader

--- torchsample/metrics.py
+++ torchsample/metrics.py
@@ -41,16 +41,11 @@ class CategoricalAccuracy(Metric):
         self.accuracy = 0
 
     def __call__(self, y_pred, y_true):
-        """
-        y_pred shape = (samples, classes)
-        y_true shape = (samples,)
-        """
         top_k = y_pred.topk(self.top_k,1)[1]
         true_k = y_true.view(len(y_true),1).expand_as(top_k)
         self.correct_count += top_k.eq(true_k).float().sum().data[0]
         self.total_count += len(y_pred)
         accuracy = 100. * float(self.correct_count) / float(self.total_count)
-
         return accuracy
 
 
@@ -69,15 +64,10 @@ class BinaryAccuracy(Metric):
         self.accuracy = 0
 
     def __call__(self, y_pred, y_true):
-        """
-        y_pred shape = (samples, classes)
-        y_true shape = (samples,)
-        """
         y_pred_round = y_pred.round().long()
         self.correct_count += y_pred_round.eq(y_true).float().sum().data[0]
         self.total_count += len(y_pred)
         accuracy = 100. * float(self.correct_count) / float(self.total_count)
-
         return accuracy
 
 
