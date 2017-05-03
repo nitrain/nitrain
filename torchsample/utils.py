34c73cdd63652d23086c6e1714b71975a11e2427
ncullen93 <ncullen.th@dartmouth.edu>
2017-05-03 15:37:59 -0400

    th.arange switch for 0.1.12

--- torchsample/utils.py
+++ torchsample/utils.py
@@ -1,9 +1,9 @@
 """
-Utility functions for torch.Tensors
+Utility functions for th.Tensors
 """
 
 import pickle
-import torch
+import torch as th
 
 import numpy as np
 
@@ -13,7 +13,7 @@ def th_allclose(x, y):
     Determine whether two torch tensors have same values
     Mimics np.allclose
     """
-    return torch.sum(torch.abs(x-y)) < 1e-5
+    return th.sum(th.abs(x-y)) < 1e-5
 
 
 def th_flatten(x):
@@ -38,7 +38,7 @@ def th_bc_flatten(x):
 
 
 def th_iterproduct(*args):
-    return torch.from_numpy(np.indices(args).reshape((len(args),-1)).T)
+    return th.from_numpy(np.indices(args).reshape((len(args),-1)).T)
 
 
 def th_iterproduct_like(x):
@@ -46,21 +46,21 @@ def th_iterproduct_like(x):
 
 
 def th_gather_nd(x, coords):
-    inds = coords.mv(torch.LongTensor(x.stride()))
-    x_gather = torch.index_select(th_flatten(x), 0, inds)
+    inds = coords.mv(th.LongTensor(x.stride()))
+    x_gather = th.index_select(th_flatten(x), 0, inds)
     return x_gather
 
 
 def th_affine2d(x, matrix, mode='bilinear', center=True):
     """
-    2D Affine image transform on torch.Tensor
+    2D Affine image transform on th.Tensor
     
     Arguments
     ---------
-    x : torch.Tensor of size (C, H, W)
+    x : th.Tensor of size (C, H, W)
         image tensor to be transformed
 
-    matrix : torch.Tensor of size (3, 3) or (2, 3)
+    matrix : th.Tensor of size (3, 3) or (2, 3)
         transformation matrix
 
     mode : string in {'nearest', 'bilinear'}
@@ -75,9 +75,9 @@ def th_affine2d(x, matrix, mode='bilinear', center=True):
     ------- 
     >>> import torch
     >>> from torchsample.utils import *
-    >>> x = torch.zeros(2,1000,1000)
+    >>> x = th.zeros(2,1000,1000)
     >>> x[:,100:1500,100:500] = 10
-    >>> matrix = torch.FloatTensor([[1.,0,-50],
+    >>> matrix = th.FloatTensor([[1.,0,-50],
     ...                             [0,1.,-50]])
     >>> xn = th_affine2d(x, matrix, mode='nearest')
     >>> xb = th_affine2d(x, matrix, mode='bilinear')
@@ -122,13 +122,13 @@ def th_affine2d(x, matrix, mode='bilinear', center=True):
 
 def th_nearest_interp2d(input, coords):
     """
-    2d nearest neighbor interpolation torch.Tensor
+    2d nearest neighbor interpolation th.Tensor
     """
     # take clamp of coords so they're in the image bounds
-    x = torch.clamp(coords[:,:,0], 0, input.size(1)-1).round()
-    y = torch.clamp(coords[:,:,1], 0, input.size(2)-1).round()
+    x = th.clamp(coords[:,:,0], 0, input.size(1)-1).round()
+    y = th.clamp(coords[:,:,1], 0, input.size(2)-1).round()
 
-    stride = torch.LongTensor(input.stride())
+    stride = th.LongTensor(input.stride())
     x_ix = x.mul(stride[1]).long()
     y_ix = y.mul(stride[2]).long()
 
@@ -143,14 +143,14 @@ def th_bilinear_interp2d(input, coords):
     """
     bilinear interpolation in 2d
     """
-    x = torch.clamp(coords[:,:,0], 0, input.size(1)-2)
+    x = th.clamp(coords[:,:,0], 0, input.size(1)-2)
     x0 = x.floor()
     x1 = x0 + 1
-    y = torch.clamp(coords[:,:,1], 0, input.size(2)-2)
+    y = th.clamp(coords[:,:,1], 0, input.size(2)-2)
     y0 = y.floor()
     y1 = y0 + 1
 
-    stride = torch.LongTensor(input.stride())
+    stride = th.LongTensor(input.stride())
     x0_ix = x0.mul(stride[1]).long()
     x1_ix = x1.mul(stride[1]).long()
     y0_ix = y0.mul(stride[2]).long()
@@ -178,7 +178,7 @@ def th_bilinear_interp2d(input, coords):
 
 def th_affine3d(x, matrix, mode='trilinear', center=True):
     """
-    3D Affine image transform on torch.Tensor
+    3D Affine image transform on th.Tensor
     """
     A = matrix[:3,:3]
     b = matrix[:3,3]
@@ -216,14 +216,14 @@ def th_affine3d(x, matrix, mode='trilinear', center=True):
 
 def th_nearest_interp3d(input, coords):
     """
-    2d nearest neighbor interpolation torch.Tensor
+    2d nearest neighbor interpolation th.Tensor
     """
     # take clamp of coords so they're in the image bounds
-    coords[:,0] = torch.clamp(coords[:,0], 0, input.size(1)-1).round()
-    coords[:,1] = torch.clamp(coords[:,1], 0, input.size(2)-1).round()
-    coords[:,2] = torch.clamp(coords[:,2], 0, input.size(3)-1).round()
+    coords[:,0] = th.clamp(coords[:,0], 0, input.size(1)-1).round()
+    coords[:,1] = th.clamp(coords[:,1], 0, input.size(2)-1).round()
+    coords[:,2] = th.clamp(coords[:,2], 0, input.size(3)-1).round()
 
-    stride = torch.LongTensor(input.stride())[1:].float()
+    stride = th.LongTensor(input.stride())[1:].float()
     idx = coords.mv(stride).long()
 
     input_flat = th_flatten(input)
@@ -235,22 +235,22 @@ def th_nearest_interp3d(input, coords):
 
 def th_trilinear_interp3d(input, coords):
     """
-    trilinear interpolation of 3D torch.Tensor image
+    trilinear interpolation of 3D th.Tensor image
     """
     # take clamp then floor/ceil of x coords
-    x = torch.clamp(coords[:,0], 0, input.size(1)-2)
+    x = th.clamp(coords[:,0], 0, input.size(1)-2)
     x0 = x.floor()
     x1 = x0 + 1
     # take clamp then floor/ceil of y coords
-    y = torch.clamp(coords[:,1], 0, input.size(2)-2)
+    y = th.clamp(coords[:,1], 0, input.size(2)-2)
     y0 = y.floor()
     y1 = y0 + 1
     # take clamp then floor/ceil of z coords
-    z = torch.clamp(coords[:,2], 0, input.size(3)-2)
+    z = th.clamp(coords[:,2], 0, input.size(3)-2)
     z0 = z.floor()
     z1 = z0 + 1
 
-    stride = torch.LongTensor(input.stride())[1:]
+    stride = th.LongTensor(input.stride())[1:]
     x0_ix = x0.mul(stride[0]).long()
     x1_ix = x1.mul(stride[0]).long()
     y0_ix = y0.mul(stride[1]).long()
@@ -292,12 +292,12 @@ def th_pearsonr(x, y):
     """
     mimics scipy.stats.pearsonr
     """
-    mean_x = torch.mean(x)
-    mean_y = torch.mean(y)
+    mean_x = th.mean(x)
+    mean_y = th.mean(y)
     xm = x.sub(mean_x)
     ym = y.sub(mean_y)
     r_num = xm.dot(ym)
-    r_den = torch.norm(xm, 2) * torch.norm(ym, 2)
+    r_den = th.norm(xm, 2) * th.norm(ym, 2)
     r_val = r_num / r_den
     return r_val
 
@@ -307,19 +307,19 @@ def th_corrcoef(x):
     mimics np.corrcoef
     """
     # calculate covariance matrix of rows
-    mean_x = torch.mean(x, 1)
+    mean_x = th.mean(x, 1)
     xm = x.sub(mean_x.expand_as(x))
     c = xm.mm(xm.t())
     c = c / (x.size(1) - 1)
 
     # normalize covariance matrix
-    d = torch.diag(c)
-    stddev = torch.pow(d, 0.5)
+    d = th.diag(c)
+    stddev = th.pow(d, 0.5)
     c = c.div(stddev.expand_as(c))
     c = c.div(stddev.expand_as(c).t())
 
     # clamp between -1 and 1
-    c = torch.clamp(c, -1.0, 1.0)
+    c = th.clamp(c, -1.0, 1.0)
 
     return c
 
@@ -334,13 +334,13 @@ def th_matrixcorr(x, y):
     (i,j) value equal to the pearsonr correlation coeff
     between column i in X and column j in Y
     """
-    mean_x = torch.mean(x, 0)
-    mean_y = torch.mean(y, 0)
+    mean_x = th.mean(x, 0)
+    mean_y = th.mean(y, 0)
     xm = x.sub(mean_x.expand_as(x))
     ym = y.sub(mean_y.expand_as(y))
     r_num = xm.t().mm(ym)
-    r_den1 = torch.norm(xm,2,0)
-    r_den2 = torch.norm(ym,2,0)
+    r_den1 = th.norm(xm,2,0)
+    r_den2 = th.norm(ym,2,0)
     r_den = r_den1.t().mm(r_den2)
     r_mat = r_num.div(r_den)
     return r_mat
@@ -351,8 +351,8 @@ def th_random_choice(a, size=None, replace=True, p=None):
     Parameters
     -----------
     a : 1-D array-like
-        If a torch.Tensor, a random sample is generated from its elements.
-        If an int, the random sample is generated as if a was torch.range(n)
+        If a th.Tensor, a random sample is generated from its elements.
+        If an int, the random sample is generated as if a was th.range(n)
     size : int, optional
         Number of samples to draw. Default is None, in which case a
         single value is returned.
@@ -372,20 +372,20 @@ def th_random_choice(a, size=None, replace=True, p=None):
         size = 1
 
     if isinstance(a, int):
-        a = torch.range(0, a-1)
+        a = th.arange(0, a)
 
     if p is None:
         if replace:
-            idx = torch.floor(torch.rand(size)*a.size(0)).long()
+            idx = th.floor(th.rand(size)*a.size(0)).long()
         else:
-            idx = torch.randperm(a.size(0))[:size]
+            idx = th.randperm(a.size(0))[:size]
     else:
         if abs(1.0-sum(p)) > 1e-3:
             raise ValueError('p must sum to 1.0')
         if not replace:
             raise ValueError('replace must equal true if probabilities given')
-        idx_vec = torch.cat([torch.zeros(round(p[i]*1000))+i for i in range(len(p))])
-        idx = (torch.floor(torch.rand(size)*999.99)).long()
+        idx_vec = th.cat([th.zeros(round(p[i]*1000))+i for i in range(len(p))])
+        idx = (th.floor(th.rand(size)*999.99)).long()
         idx = idx_vec[idx].long()
     return a[idx]
 
