943753c968fe7c0cbafc6e44d60f1b28b42e997b
ncullen93 <ncullen.th@dartmouth.edu>
2017-05-03 09:43:04 -0400

    affine2d multichannel support

--- torchsample/utils.py
+++ torchsample/utils.py
@@ -82,25 +82,29 @@ def th_affine2d(x, matrix, mode='bilinear', center=True):
     >>> xn = th_affine2d(x, matrix, mode='nearest')
     >>> xb = th_affine2d(x, matrix, mode='bilinear')
     """
-    A = matrix[:2,:2]
-    b = matrix[:2,2]
+    if matrix.dim() == 2:
+        matrix = matrix.view(-1,2,3)
+
+    A_batch = matrix[:,:,:2]
+    if A_batch.size(0) != x.size(0):
+        A_batch = A_batch.repeat(x.size(0),1,1)
+    b_batch = matrix[:,:,2].unsqueeze(1)
 
     # make a meshgrid of normal coordinates
-    #coords = th_iterproduct_like(x).float()
-    coords = th_iterproduct(x.size(1),x.size(2)).float()
+    _coords = th_iterproduct(x.size(1),x.size(2))
+    coords = _coords.unsqueeze(0).repeat(x.size(0),1,1).float()
 
     if center:
         # shift the coordinates so center is the origin
-        coords[:,0] = coords[:,0] - (x.size(1) / 2. + 0.5)
-        coords[:,1] = coords[:,1] - (x.size(2) / 2. + 0.5)
-    
+        coords[:,:,0] = coords[:,:,0] - (x.size(1) / 2. + 0.5)
+        coords[:,:,1] = coords[:,:,1] - (x.size(2) / 2. + 0.5)
     # apply the coordinate transformation
-    new_coords = coords.mm(A.t().contiguous()) + b.expand_as(coords)
+    new_coords = coords.bmm(A_batch.transpose(1,2)) + b_batch.expand_as(coords)
 
     if center:
         # shift the coordinates back so origin is origin
-        new_coords[:,0] = new_coords[:,0] + (x.size(1) / 2. + 0.5)
-        new_coords[:,1] = new_coords[:,1] + (x.size(2) / 2. + 0.5)
+        new_coords[:,:,0] = new_coords[:,:,0] + (x.size(1) / 2. + 0.5)
+        new_coords[:,:,1] = new_coords[:,:,1] + (x.size(2) / 2. + 0.5)
 
     # map new coordinates using bilinear interpolation
     if mode == 'nearest':
@@ -116,15 +120,16 @@ def th_nearest_interp2d(input, coords):
     2d nearest neighbor interpolation torch.Tensor
     """
     # take clamp of coords so they're in the image bounds
-    coords[:,0] = torch.clamp(coords[:,0], 0, input.size(1)-1).round()
-    coords[:,1] = torch.clamp(coords[:,1], 0, input.size(2)-1).round()
+    x = torch.clamp(coords[:,:,0], 0, input.size(1)-1).round()
+    y = torch.clamp(coords[:,:,1], 0, input.size(2)-1).round()
 
-    stride = torch.LongTensor(input.stride())[1:].float()
-    idx = coords.mv(stride).long()
+    stride = torch.LongTensor(input.stride())
+    x_ix = x.mul(stride[1]).long()
+    y_ix = y.mul(stride[2]).long()
 
-    input_flat = th_flatten(input)
+    input_flat = input.view(input.size(0),-1).contiguous()
 
-    mapped_vals = input_flat[idx]
+    mapped_vals = input_flat.gather(1, x_ix.add(y_ix))
 
     return mapped_vals.view_as(input)
 
@@ -133,36 +138,34 @@ def th_bilinear_interp2d(input, coords):
     """
     bilinear interpolation in 2d
     """
-    # take clamp then floor/ceil of x coords
-    x = torch.clamp(coords[:,0], 0, input.size(1)-2).contiguous().view(-1)
+    x = torch.clamp(coords[:,:,0], 0, input.size(1)-2)
     x0 = x.floor()
     x1 = x0 + 1
-    # take clamp then floor/ceil of y coords
-    y = torch.clamp(coords[:,1], 0, input.size(2)-2).contiguous().view(-1)
+    y = torch.clamp(coords[:,:,1], 0, input.size(2)-2)
     y0 = y.floor()
     y1 = y0 + 1
 
-    stride = torch.LongTensor(input.stride()[1:])
-    x0_ix = x0.mul(stride[0]).long()
-    x1_ix = x1.mul(stride[0]).long()
-    y0_ix = y0.mul(stride[1]).long()
-    y1_ix = y1.mul(stride[1]).long()
+    stride = torch.LongTensor(input.stride())
+    x0_ix = x0.mul(stride[1]).long()
+    x1_ix = x1.mul(stride[1]).long()
+    y0_ix = y0.mul(stride[2]).long()
+    y1_ix = y1.mul(stride[2]).long()
 
-    input_flat = th_flatten(input)
+    input_flat = input.view(input.size(0),-1).contiguous()
 
-    vals_00 = input_flat[x0_ix+y0_ix]
-    vals_10 = input_flat[x1_ix+y0_ix]
-    vals_01 = input_flat[x0_ix+y1_ix]
-    vals_11 = input_flat[x1_ix+y1_ix]
-
+    vals_00 = input_flat.gather(1, x0_ix.add(y0_ix))
+    vals_10 = input_flat.gather(1, x1_ix.add(y0_ix))
+    vals_01 = input_flat.gather(1, x0_ix.add(y1_ix))
+    vals_11 = input_flat.gather(1, x1_ix.add(y1_ix))
+    
     xd = x - x0
     yd = y - y0
-    xm1 = 1 - xd
-    ym1 = 1 - yd
+    xm = 1 - xd
+    ym = 1 - yd
 
-    x_mapped = (vals_00.mul(xm1).mul(ym1) +
-                vals_10.mul(xd).mul(ym1) +
-                vals_01.mul(xm1).mul(yd) +
+    x_mapped = (vals_00.mul(xm).mul(ym) +
+                vals_10.mul(xd).mul(ym) +
+                vals_01.mul(xm).mul(yd) +
                 vals_11.mul(xd).mul(yd))
 
     return x_mapped.view_as(input)
