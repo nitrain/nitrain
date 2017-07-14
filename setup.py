651cc796e060c63db3f97f63c69c2e267ba17fb4
Nick Cullen <ncullen.th@dartmouth.edu>
2017-07-14 11:26:59 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(name='torchsample',
       version='0.1.3',
-      description='Sampling for Pytorch',
+      description='High-Level Training, Augmentation, and Sampling for Pytorch',
       author='NC Cullen',
       author_email='nickmarch31@yahoo.com',
       packages=find_packages()
