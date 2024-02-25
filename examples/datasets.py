f74ad91c0f70672dba261ed7a117a22120af3289
ncullen93 <nicholas.cullen@med.lu.se>
2024-02-25 18:29:56 +0100

    create simple data loader

--- examples/datasets.py
+++ examples/datasets.py
@@ -9,21 +9,32 @@ import pandas as pd
 from nitrain import utils, data
 
 download = utils.fetch_datalad('ds004711')
-        
+
+# use a Datalad/BIDS folder -> create a FolderDataset 
 ds = data.FolderDataset(path = download.path, 
-                         layout = 'bids',
-                         x_config = {'suffix': 'T1w', 'run': [None, '01']},
-                         y_config = {'column': 'age'})
+                        layout = 'bids',
+                        x_config = {'suffix': 'T1w', 'run': [None, '01']},
+                        y_config = {'column': 'age'})
 
 # filter data based on participants metadata file
-ds_filtered = ds.filter('age < 18')
-ds_females = ds.filter('gender == "female"')
+ds_under40 = ds.filter('age < 40')
+ds_males = ds.filter('gender == "male"')
 
-# load some images into memory
-x, y = ds.load(n=5)
+# access image filenames via index (note this does not read in data)
+x, y = ds[:5]
 
-# access data via index (note this does not read in data)
-x, y = ds[0]
+# load images into memory
+x, y = ds.load(n=7)
 
+## save filenames to csv file -> create a CSVDataset
+#ds_csv = data.CSVDataset('dataset.csv')
 
+## use in-memory images -> create a MemoryDataset
+ds_memory = data.MemoryDataset(x, y)
+
+loader = data.DatasetLoader(ds_memory, batch_size = 2)
+
+# loop through all batches (one epoch)
+for x, y in loader:
+    print(y)
 
--- nitrain/data/loaders.py
+++ nitrain/data/loaders.py
@@ -1,11 +1,42 @@
 # loaders help you feed your data into deep learning models with transforms
+import math
 
 class DatasetLoader:
     
-    def __init__(self, dataset, x_transforms=None, y_transforms=None, co_transforms=None):
+    def __init__(self, dataset, batch_size, x_transforms=None, y_transforms=None, co_transforms=None):
         self.dataset = dataset
+        self.batch_size = batch_size
+    
+    def __iter__(self):
+       batch_idx = 0
+       batch_size = self.batch_size
+       dataset = self.dataset
+       
+       while batch_idx < self.n_batches():
+           data_indices = list(range(batch_idx*batch_size, min((batch_idx+1)*batch_size, len(dataset))))
+           x, y = self.dataset[data_indices]
+           yield x, y
+           batch_idx += 1
+    
+    def n_batches(self):
+        return math.ceil(len(self.dataset) / self.batch_size)
     
     @property
     def input_shape(self):
         # load the first input, perform any transforms, and get shape
         return self.dataset[0][0].shape
+
+class Counter:
+    def __init__(self, start, end):
+        self.num = start
+        self.end = end
+ 
+    def __iter__(self):
+        return self
+ 
+    def __next__(self): 
+        if self.num > self.end:
+            raise StopIteration
+        else:
+            self.num += 1
+            return self.num - 1
