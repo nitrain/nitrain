b400041b9e4bba85a29519325c7cb4ac67a9d91d
ncullen93 <nicholas.cullen@med.lu.se>
2024-02-25 11:29:13 +0100

    add file dataset

--- examples/download_data.py
+++ examples/download_data.py
@@ -6,5 +6,14 @@ from nitrain.utils import download_data
 # will go to downloads directory
 download_data('ds003826')
 
-# check this out using bids
+# get some data
+# ds = dl.Dataset(localizer_path)
 
+# todo: use bids instead of glob
+# file_list = glob.glob(os.path.join(localizer_path, 'derivatives', 'fmriprep', '*', 'func', '*task-localizer_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz'))
+
+# result = ds.get(file_list[0])
+
+
+
+# https://dartbrains.org/content/Download_Data.html
