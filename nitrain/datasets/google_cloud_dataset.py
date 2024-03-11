bad2dd8beb7cfcf1d41a9eb355c19ce22d57cba8
ncullen93 <nicholas.cullen@med.lu.se>
2024-03-11 17:44:53 +0100

    support fuse in gcs dataset

--- nitrain/datasets/google_cloud_dataset.py
+++ nitrain/datasets/google_cloud_dataset.py
@@ -1,5 +1,6 @@
 import warnings
 import os
+import glob
 import tempfile
 import pandas as pd
 import numpy as np
@@ -23,7 +24,7 @@ class GoogleCloudDataset:
                  x_transforms=None,
                  y_transforms=None,
                  credentials=None,
-                 use_fuse=False):
+                 fuse=False):
         """
         Initialize a nitrain dataset consisting of local filepaths.
         
@@ -38,34 +39,56 @@ class GoogleCloudDataset:
                                          y={'file': 'participants.tsv', 'column': 'age'},
                                          credentials='/Users/ni5875cu/Desktop/ants.dev/engine/deep-dynamics-415608-4046316ec2f1.json')
         """
-        if isinstance(credentials, str):
-            credentials = service_account.Credentials.from_service_account_file(credentials)
-        if credentials is not None:
-            storage_client = storage.Client(credentials=credentials)
-        bucket_client = storage_client.bucket(bucket)
-
         x_config = x
         y_config = y
         
-        # GET X
-        if not base_dir.endswith('/'): base_dir += '/'
-        x_blobs = storage_client.list_blobs(bucket, match_glob=f'{base_dir}{x_config["pattern"]}')
-        x = list([blob.name.replace(base_dir, '') for blob in x_blobs])
-        if 'exclude' in x_config.keys():
-            x = [file for file in x if not fnmatch(file, x_config['exclude'])]
-        x_ids = [xx.split('/')[0] for xx in x]
-        x = [os.path.join(base_dir, file) for file in sorted(x)]
-        
-        if len(x) == 0:
-            raise Exception('Did not find any x values corresponding to the x config.')
+        if fuse:
+            base_dir = os.path.join('/gcs/', bucket, base_dir)
+            if not base_dir.endswith('/'): 
+                base_dir += '/'
+            
+            # GET X
+            x = glob.glob(x_config['pattern'], root_dir=base_dir)
+            print(x)
+            if 'exclude' in x_config.keys():
+                x = [file for file in x if not fnmatch(file, x_config['exclude'])]
+            x_ids = [xx.split('/')[0] for xx in x]
+            x = [os.path.join('/gcs/', bucket, base_dir, file) for file in sorted(x)]
+            
+            if len(x) == 0:
+                raise Exception('Did not find any x values corresponding to the x config.')
 
-        # GET Y
-        y_file = os.path.join(base_dir, y_config['file'])
-        y_blob = bucket_client.blob(y_file)
-        tmp_file = tempfile.NamedTemporaryFile()
-        y_blob.download_to_filename(tmp_file.name)
-        y_df = pd.read_csv(tmp_file.name, sep='\t')
-        tmp_file.close()
+            # GET Y
+            y_file = os.path.join(base_dir, y_config['file'])
+            y_df = pd.read_csv(y_file, sep='\t')
+            print(y_df)
+            
+        else:
+            if isinstance(credentials, str):
+                credentials = service_account.Credentials.from_service_account_file(credentials)
+            if credentials is not None:
+                storage_client = storage.Client(credentials=credentials)
+            bucket_client = storage_client.bucket(bucket)
+        
+            # GET X
+            if not base_dir.endswith('/'): base_dir += '/'
+            x_blobs = storage_client.list_blobs(bucket, match_glob=f'{base_dir}{x_config["pattern"]}')
+            x = list([blob.name.replace(base_dir, '') for blob in x_blobs])
+            if 'exclude' in x_config.keys():
+                x = [file for file in x if not fnmatch(file, x_config['exclude'])]
+            x_ids = [xx.split('/')[0] for xx in x]
+            x = [os.path.join(base_dir, file) for file in sorted(x)]
+            
+            if len(x) == 0:
+                raise Exception('Did not find any x values corresponding to the x config.')
+
+            # GET Y
+            y_file = os.path.join(base_dir, y_config['file'])
+            y_blob = bucket_client.blob(y_file)
+            tmp_file = tempfile.NamedTemporaryFile()
+            y_blob.download_to_filename(tmp_file.name)
+            y_df = pd.read_csv(tmp_file.name, sep='\t')
+            tmp_file.close()
 
         # match x and y ids
         p_col = y_df.columns[0] # assume participant id is first row
@@ -82,9 +105,9 @@ class GoogleCloudDataset:
         x = [x[idx] for idx in range(len(x)) if x_ids[idx] in y_ids]
         x_ids = y_ids
         
-        self.credentials = credentials
-        self.storage_client = storage_client
-        self.bucket_client = bucket_client
+        self.credentials = credentials if credentials else None
+        self.storage_client = storage_client if storage_client else None
+        self.bucket_client = bucket_client if bucket_client else None
         self.bucket = bucket
         self.base_dir = base_dir
         self.x_config = x_config
@@ -94,12 +117,11 @@ class GoogleCloudDataset:
         self.x_transforms = x_transforms
         self.y_transforms = y_transforms
         self.tmp_dir = tempfile.TemporaryDirectory()
-        self.use_fuse = use_fuse
+        self.fuse = fuse
 
     def __getitem__(self, idx):
         files = self.x[idx]
         if not isinstance(idx, slice):
-            print('not a slice')
             files = [files]
         y = self.y[idx]
         
@@ -108,10 +130,10 @@ class GoogleCloudDataset:
         
         x = []
         for file in files:
+            print(file)
             # if on vertex, file will be available to access. otherwise, download it to local tmp dir.
-            if self.use_fuse:
-                local_filepath = os.path.join('/gcs/', self.bucket, file)
-                print(local_filepath)
+            if self.fuse:
+                local_filepath = file
             else:
                 local_filepath = os.path.join(self.tmp_dir.name, file)
                 if not os.path.exists(local_filepath):
