faee62d4ebe8fefc3592bb2465141ae3765116a8
ncullen93 <nicholas.cullen@med.lu.se>
2024-03-13 12:43:12 +0100

    organize samplers

---
+++ nitrain/samplers/patch_sampler.py
@@ -0,0 +1,16 @@
+"""
+        image_patches = list()
+        for ii in range(len(images)):
+            image_patches.append(antspynet.extract_image_patches(images[ii], 
+                                                        patch_size=patch_size,
+                                                        max_number_of_patches=number_of_patches_per_image,
+                                                        mask_image=seg_mask,
+                                                        random_seed=random_seed,
+                                                        return_as_array=True))
+        seg_patches = antspynet.extract_image_patches(seg, 
+                                                        patch_size=patch_size,
+                                                        max_number_of_patches=number_of_patches_per_image,
+                                                        mask_image=seg_mask,
+                                                        random_seed=random_seed,
+                                                        return_as_array=True)
+"""
--- nitrain/transforms/structural_transforms.py
+++ nitrain/transforms/structural_transforms.py
@@ -168,23 +168,3 @@ class Pad(BaseTransform):
                                     pad_width=self.width,
                                     value=self.value)
         return new_image
-
-
-class ExtractPatches:
-    """
-            image_patches = list()
-            for ii in range(len(images)):
-                image_patches.append(antspynet.extract_image_patches(images[ii], 
-                                                         patch_size=patch_size,
-                                                         max_number_of_patches=number_of_patches_per_image,
-                                                         mask_image=seg_mask,
-                                                         random_seed=random_seed,
-                                                         return_as_array=True))
-            seg_patches = antspynet.extract_image_patches(seg, 
-                                                          patch_size=patch_size,
-                                                          max_number_of_patches=number_of_patches_per_image,
-                                                          mask_image=seg_mask,
-                                                          random_seed=random_seed,
-                                                          return_as_array=True)
-    """
-    pass
