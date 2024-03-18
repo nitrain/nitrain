67b463ae8f0031da4dfc87de956b522704e35a6b
ncullen93 <nicholas.cullen@med.lu.se>
2024-03-18 10:57:00 +0100

    add model trainer class for keras

--- nitrain/trainers/model_trainer.py
+++ nitrain/trainers/model_trainer.py
@@ -14,12 +14,83 @@ class ModelTrainer:
     >>> loader = DatasetLoader(data, batch_size=32)
     >>> model_fn = fetch_architecture('autoencoder')
     >>> model = model_fn((120, 60, 30))
-    >>> trainer = ModelTrainer(model)
+    >>> trainer = ModelTrainer(model, task='image_to_image')
     >>> trainer.fit(loader, epochs=10)
     """
     
-    def __init__(self, model):
-        self.model = model
+    def __init__(self, 
+                 model,
+                 task,
+                 optimizer=None,
+                 loss=None,
+                 metrics=None):
+        """
+        Create a model trainer with sensible defaults or user-defined
+        settings from a pytorch, keras, or tensorflow model.
         
-    def fit(self, loader, epochs):
+        Arguments
+        ---------
+        task: string
+            options: regression, classification, image_to_image
+            
+        # callbacks
+        # regularizers
+        # initializers
+        # constraints
+        # metrics
+        # losses
+        """
+        
+        self.model = model
+        if task == 'regression':
+            optimizer = 'adam' if optimizer is None else optimizer
+            loss = 'mse' if loss is None else loss
+            metrics = ['mse'] if metrics is None else metrics
+        elif task == 'classification':
+            optimizer = 'adam' if optimizer is None else optimizer
+            if model.output_shape[1] == 1:
+                loss = 'categorical_crossentropy' if loss is None else loss
+            else:
+                loss = 'binary_crossentropy' if loss is None else loss
+            metrics = ['accuracy'] if metrics is None else metrics
+        
+        self.task = task
+        self.optimizer = optimizer
+        self.loss = loss
+        self.metrics = metrics
+                
+        framework = infer_framework(model)
+        self.framework = framework
+        
+        if framework == 'keras':
+            self.model.compile(optimizer='adam',
+                               loss='mse',
+                               metrics=['mse'])
+        
+    def fit(self, loader):
+        if self.framework == 'keras':
+            return self.model.fit(loader)
+
+    def evaluate(self, loader):
+        if self.framework == 'keras':
+            return self.model.evaluate(loader)
+    
+    def predict(self, loader):
+        if self.framework == 'keras':
+            return self.model.predict(loader)
+    
+    def summary(self):
+        if self.framework == 'keras':
+            return self.model.summary()
+    
+    def save(self):
         pass
+
+
+def infer_framework(model):
+    model_type = str(type(model))
+    if 'keras' in model_type:
+        return 'keras'
+    if 'torch' in model_type:
+        return 'torch'
+    
