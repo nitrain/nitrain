389dc010a80619fd07e42d4f223c6859bfca823e
ncullen93 <nicholas.cullen@med.lu.se>
2024-03-26 11:57:36 +0100

    ENH: add tests for samplers

--- tests/test_samplers.py
+++ tests/test_samplers.py
@@ -11,6 +11,84 @@ import ants
 from nitrain import datasets, loaders, samplers, transforms as tx
 
 
+
+class TestClass_BaseSampler(unittest.TestCase):
+    def setUp(self):
+        img = ants.image_read(ants.get_data('mni'))
+        x = [img for _ in range(5)]
+        y = list(range(5))
+        self.dataset = datasets.MemoryDataset(x, y)
+
+    def tearDown(self):
+        pass
+    
+    def test_standard(self):
+        x_raw, y_raw = self.dataset[:3]
+        sampler = samplers.BaseSampler(sub_batch_size=3)
+        
+        sampled_batch = sampler(x_raw, y_raw)
+        
+        x_batch, y_batch = next(iter(sampled_batch))
+        self.assertTrue(len(x_batch)==3)
+        
+        self.assertTrue(len(y_batch)==3)
+        # no shuffle
+        self.assertTrue(y_batch==[0,1,2])
+
+class TestClass_PatchSampler(unittest.TestCase):
+    def setUp(self):
+        img = ants.image_read(ants.get_data('r16')).resample_image((4,4))
+        x = [img for _ in range(5)]
+        y = list(range(5))
+        self.dataset = datasets.MemoryDataset(x, y)
+
+    def tearDown(self):
+        pass
+    
+    def test_standard(self):
+        x_raw, y_raw = self.dataset[:3]
+        sampler = samplers.PatchSampler(patch_size=(24,24),
+                                        stride=(24,24),
+                                        sub_batch_size=4)
+        
+        sampled_batch = sampler(x_raw, y_raw)
+        
+        x_batch, y_batch = next(iter(sampled_batch))
+        self.assertTrue(len(x_batch)==4)
+        self.assertTrue(x_batch[0].shape==(24,24))
+        
+        self.assertTrue(len(y_batch)==4)
+        # no shuffle
+        self.assertTrue(all(y_batch==0))
+
+class TestClass_SlicePatchSampler(unittest.TestCase):
+    def setUp(self):
+        img = ants.image_read(ants.get_data('mni')).resample_image((4,4,4))
+        x = [img for _ in range(5)]
+        y = list(range(5))
+        self.dataset = datasets.MemoryDataset(x, y)
+
+    def tearDown(self):
+        pass
+    
+    def test_standard(self):
+        x_raw, y_raw = self.dataset[:3]
+        sampler = samplers.SlicePatchSampler(patch_size=(32,32), 
+                                             stride=(32,32), 
+                                             axis=2, 
+                                             sub_batch_size=4)
+        
+        sampled_batch = sampler(x_raw, y_raw)
+        
+        x_batch, y_batch = next(iter(sampled_batch))
+        self.assertTrue(len(x_batch)==4)
+        self.assertTrue(x_batch[0].dimension==2)
+        self.assertTrue(x_batch[0].shape==(32,32))
+        
+        self.assertTrue(len(y_batch)==4)
+        # no shuffle
+        self.assertTrue(all(y_batch==0))
+        
 class TestClass_SliceSampler(unittest.TestCase):
     def setUp(self):
         img = ants.image_read(ants.get_data('mni'))
