2225bc93381d76ad9b6935a4a82c00903770e693
ncullen93 <nicholas.cullen@med.lu.se>
2024-04-26 09:37:00 +0200

    update readme + tests

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,19 +1,15 @@
 import os
 import unittest
-from main import run_tests
 
-from tempfile import mktemp, mkdtemp, NamedTemporaryFile
+from tempfile import mkdtemp
 import shutil
-
-import base64
-import json
 import pandas as pd
-import numpy as np
-import numpy.testing as nptest
 
 import ntimage as nti
 import nitrain as nt
-from nitrain import readers
+from nitrain import readers, transforms as tx
+
+from main import run_tests
 
 class TestClass_Dataset(unittest.TestCase):
     def setUp(self):
@@ -133,6 +129,114 @@ class TestClass_CSVDataset(unittest.TestCase):
                 inputs=readers.ColumnReader(column='filenames_3d', is_image=True),
                 outputs=readers.ColumnReader(column='age')
             )
+
+class TestClass_FolderDataset(unittest.TestCase):
+    def setUp(self):
+        # set up directory
+        tmp_dir = mkdtemp()
+        self.tmp_dir = tmp_dir
+        img2d = nti.load(nti.example_data('r16'))
+        img3d = nti.load(nti.example_data('mni'))
+        for i in range(5):
+            sub_dir = os.path.join(tmp_dir, f'sub_{i}')
+            os.mkdir(sub_dir)
+            nti.save(img2d, os.path.join(sub_dir, 'img2d.nii.gz'))
+            nti.save(img3d, os.path.join(sub_dir, 'img3d.nii.gz'))
         
+        # write csv file
+        ids = [f'sub_{i}' for i in range(5)]
+        age = [i + 50 for i in range(5)]
+        df = pd.DataFrame({'sub_id': ids, 'age': age})
+        df.to_csv(os.path.join(tmp_dir, 'participants.csv'), index=False)
+        
+    def tearDown(self):
+        shutil.rmtree(self.tmp_dir)
+    
+    def test_2d(self):
+        dataset = nt.FolderDataset(
+            base_dir=self.tmp_dir,
+            x={'pattern': '*/img2d.nii.gz'},
+            y={'file': 'participants.csv', 'column': 'age'}
+        )
+        self.assertTrue(len(dataset.x) == 5)
+        self.assertTrue(len(dataset.y) == 5)
+        
+        x, y = dataset[:2]
+        self.assertTrue(len(x) == 2)
+        
+        # test repr
+        r = dataset.__repr__()
+        
+    def test_double_image_input(self):
+        dataset = nt.FolderDataset(
+            base_dir=self.tmp_dir,
+            x=[{'pattern': '*/img2d.nii.gz'},{'pattern': '*/img2d.nii.gz'}],
+            y={'file': 'participants.csv', 'column': 'age'}
+        )
+        self.assertTrue(len(dataset.x) == 5)
+        self.assertTrue(len(dataset.x[0]) == 2)
+        self.assertTrue(len(dataset.y) == 5)
+        
+        x, y = dataset[:2]
+        self.assertTrue(len(x) == 2)
+        self.assertTrue(len(x[0]) == 2)
+
+    def test_2d_image_to_image(self):
+        dataset = nt.FolderDataset(
+            base_dir=self.tmp_dir,
+            x={'pattern': '*/img2d.nii.gz'},
+            y={'pattern': '*/img2d.nii.gz'}
+        )
+        self.assertTrue(len(dataset.x) == 5)
+        self.assertTrue(len(dataset.y) == 5)
+        
+        x, y = dataset[:2]
+        self.assertTrue(len(x) == 2)
+
+    def test_2d_image_to_image_and_x_transforms(self):
+        dataset = nt.FolderDataset(
+            base_dir=self.tmp_dir,
+            x={'pattern': '*/img2d.nii.gz'},
+            y={'pattern': '*/img2d.nii.gz'},
+            transforms={'x': [tx.Resample((69,69))]}
+        )
+        self.assertTrue(len(dataset.x) == 5)
+        self.assertTrue(len(dataset.y) == 5)
+        
+        x, y = dataset[:2]
+        self.assertTrue(len(x) == 2)
+        
+        self.assertTrue(x[0].shape==(69,69))
+        self.assertTrue(y[0].shape!=(69,69))
+        
+    def test_2d_image_to_image_and_co_transforms(self):
+        dataset = nt.FolderDataset(
+            base_dir=self.tmp_dir,
+            x={'pattern': '*/img2d.nii.gz'},
+            y={'pattern': '*/img2d.nii.gz'},
+            transforms={'co': [tx.Resample((69,69))]}
+        )
+        self.assertTrue(len(dataset.x) == 5)
+        self.assertTrue(len(dataset.y) == 5)
+        
+        x, y = dataset[:2]
+        self.assertTrue(len(x) == 2)
+        
+        self.assertTrue(x[0].shape==(69,69))
+        self.assertTrue(y[0].shape==(69,69))
+        
+    def test_3d(self):
+        dataset = nt.FolderDataset(
+            base_dir=self.tmp_dir,
+            x={'pattern': '*/img3d.nii.gz'},
+            y={'file': 'participants.csv', 'column': 'age'}
+        )
+        self.assertTrue(len(dataset.x) == 5)
+        self.assertTrue(len(dataset.y) == 5)
+        
+        x, y = dataset[:2]
+        self.assertTrue(len(x) == 2)
+
+
 if __name__ == '__main__':
     run_tests()
