4526889ee9dbf48d89482044bc857ad5e48a5b0d
ncullen93 <nicholas.cullen@med.lu.se>
2024-05-04 16:29:37 +0200

    update base transform

--- nitrain/transforms/base.py
+++ nitrain/transforms/base.py
@@ -13,15 +13,6 @@ class BaseTransform:
     def __call__(self, *inputs):
         raise NotImplementedError
 
-    def sample(self, image, n=10):
-        res = []
-        for _ in range(n):
-            res.append(self.__call__(image))
-        return res
-        
-    def __call__(self, *inputs):
-        raise NotImplementedError
-
     def __repr__(self):
         raise NotImplementedError
 
