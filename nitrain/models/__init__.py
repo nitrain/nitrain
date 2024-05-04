63fc37c82bc9672aaec99eb29528d6a04b776a01
ncullen93 <nicholas.cullen@med.lu.se>
2024-05-04 18:11:24 +0200

    remove unused file

--- nitrain/models/__init__.py
+++ nitrain/models/__init__.py
@@ -1,4 +1,3 @@
 # functions for loading and sharing pre-trained neuroimaging-based deep learning models
 
 from .fetch_architecture import *
-from .load import load
--- nitrain/models/load.py
+++
@@ -1,6 +0,0 @@
-
-
-def load(path):
-    import tensorflow as tf
-    model = tf.keras.models.load_model(path)
-    return model
